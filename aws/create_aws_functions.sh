d595afe82213acd9f8bfb858c83a7d30d1257541
Mikko Pervilä <mikko.pervila@gmail.com>
2016-12-22 19:42:35 +0200

    Added the regions explicitly, they're assumed from based on conf sometimes

--- aws/create_aws_functions.sh
+++ aws/create_aws_functions.sh
@@ -3,14 +3,14 @@
 zip TaskList.zip TaskList.py
 
 # list
-aws lambda create-function --function-name TaskListGET --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListQuery --handler TaskList.list --runtime python2.7
+aws lambda create-function --region eu-west-1 --function-name TaskListGET --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListQuery --handler TaskList.list --runtime python2.7
 
 # update
-aws lambda create-function --function-name TaskListPUT --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListPut --handler TaskList.add --runtime python2.7
+aws lambda create-function --region eu-west-1 --function-name TaskListPUT --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListPut --handler TaskList.add --runtime python2.7
 
 # add (same as update)
-aws lambda create-function --function-name TaskListPOST --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListUpdate --handler TaskList.add --runtime python2.7
+aws lambda create-function --region eu-west-1 --function-name TaskListPOST --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListUpdate --handler TaskList.add --runtime python2.7
 
 # delete 
-aws lambda create-function --function-name TaskListDELETE --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListDelete --handler TaskList.delete --runtime python2.7
+aws lambda create-function --region eu-west-1 --function-name TaskListDELETE --zip-file fileb://TaskList.zip --role arn:aws:iam::312445988018:role/taskListDelete --handler TaskList.delete --runtime python2.7
 
